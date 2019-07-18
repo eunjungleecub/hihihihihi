3d55508254aabc06860a11b4063d494ddbccb984
Jessi Sassy <asaprocky123@likelion.org>
2019-07-19 04:15:20 +0900

    altered field

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True) #사용자가 직접 입력하지 않아도 자동으로 시간 받아오기
     content = models.TextField(default='') #default='', content에 아무것도 안써도 null 에러 안나게 하기
 
