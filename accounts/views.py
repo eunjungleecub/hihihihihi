d9bda72f55f8184c533dde5c0b768e045b94e98d
Jessi Sassy <asaprocky123@likelion.org>
2019-07-21 13:50:39 +0900

    members

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
-from django.core.exceptions import ObjectDoesNotExist
+# Create your views here.
+
 
 def signup(request):
-    if request.method=='POST':
-        if request.POST['password1']==request.POST['password2']: #user가 입력한 password가 회원가입할때 받은 password가 일치하느냐
-            try: 
-                user = User.objects.get(username=request.POST['username']) #username 
-                return render(request, 'signup.html', {'error':'이미 사용중인 이름입니다.'})
-            except User.ObjectDoesnotExist: # 
-                user=User.objects.create_user( #
-                    username=request.POST['username'], password=request.POST['password1'])
-                auth.login(request,user)
-                return redirect('home')
-        else:
-            return render(request,'signup.html', {'error':'비밀번호가 일치하지 않습니다.'})
-    else:
-        return render(request,'signup.html')
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('home')
+    return render(request, 'signup.html')
 
 def login(request):
     if request.method=='POST':
